481aad875f840a4a69847be29d3586e7cb898061
yangyuxin325 <yang325@live.cn>
2018-06-05 17:16:57 +0800

    change import WendaApplication

--- src/main/java/com/nowcoder/WendaApplication.java
+++ src/main/java/com/nowcoder/WendaApplication.java
@@ -1,6 +1,5 @@
 package com.nowcoder;
 
-import com.nowcoder.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
